d0c4eeee8d9c5226c15fe0071c4f4ff0ceadc9b9
kbrousse <kbrousse@2D5.42angouleme.fr>
2026-05-12 11:38:37 +0200

    Prevent snake from colliding with own tail

--- sources/collision/is_snake_collision_self.py
+++ sources/collision/is_snake_collision_self.py
@@ -2,5 +2,8 @@ from pygame import Rect
 
 
 def is_snake_collision_self(snake_pos: Rect, segments: list[Rect]) -> bool:
-    """Checks if the snake collides with any segment of its own body"""
-    return any(snake_pos.colliderect(segment) for segment in segments)
+    """Checks if the snake collides with any segment of its own body apart
+    from its tail (segments[0]). We do not check collision with the tail
+    for the tail moves exactly as the head moves, which makes them
+    impossible to collide."""
+    return any(snake_pos.colliderect(segment) for segment in segments[1:])
